83c0ff7323acfd6a7adc5cfb31e495f3621dc32f
Leandro Curzi <lcurzi@MODUSAGENCY.COM>
2018-02-06 18:54:23 -0300

    changes

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 let math = require("mathjs")
 
 class Customer{
-    static get(id){
-        if(!id || id.trim().length == 0) return null;
 
-        return {
-            customer_name: "Leandro Curzi",
-            customer_location: "Buenos Aires",
-            customer_age: math.sqrt(64)
+    /**
+     * Generic handler for all GET requests in this service
+     * @param {*} query = {
+     *      type: String,
+     *      param: Object
+     * } 
+     */
+    static get(query){
+        let result = {status: false, resp: null};
+
+        switch(query.type){
+            case "GET_BY_ID": 
+                result.resp = this.getById(query.param);
+                result.status = true;
+            break;
+
+            case "GET_BY_PHONE":
+                result.resp = this.getByPhone(query.param);
+                result.status = true;
+            break;
         }
+
+        return result;
+    }
+
+    static getById(id){
+        return {name: "Leandro Curzi", phone: "123-456-7890"};
+    }
+
+    static getByPhone(phoneNum){
+        return {name: "Leandro Curzi", id: "abc1234"};
     }
 }
 
